7f841e0719136e7ff69f34be54fc05036d0dee97
Fernando Barbosa <fernandobarbosa1697@gmail.com>
2024-03-05 11:03:17 -0300

    authenticate unit tests

---
+++ src/use-cases/authenticate-org.use-case.spec.ts
@@ -0,0 +1,54 @@
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs.repository'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { makeOrg } from '@tests/factories/make-org.factory'
+import { hash } from 'bcryptjs'
+import { AuthenticateOrgUseCase } from './authenticate-org.use-case'
+import { InvalidCredentialsError } from './errors/invalid-credentials.error'
+
+let orgsRepository: InMemoryOrgsRepository
+let sut: AuthenticateOrgUseCase
+
+describe('Authenticate Org Use Case', () => {
+  beforeEach(() => {
+    orgsRepository = new InMemoryOrgsRepository()
+    sut = new AuthenticateOrgUseCase(orgsRepository)
+  })
+
+  it('should be able to authenticate an org', async () => {
+    const password = '123456'
+
+    const org = await orgsRepository.create(
+      makeOrg({ password: await hash(password, 8) }),
+    )
+
+    const { org: authenticatedOrg } = await sut.execute({
+      email: org.email,
+      password,
+    })
+
+    expect(authenticatedOrg).toEqual(org)
+  })
+  it('should not be able to authenticate with wrong email', async () => {
+    await expect(() =>
+      sut.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+
+  it('should not be able to authenticate with wrong password', async () => {
+    const password = '123456'
+
+    const org = await orgsRepository.create(
+      makeOrg({ password: await hash(password, 8) }),
+    )
+
+    await expect(() =>
+      sut.execute({
+        email: org.email,
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(InvalidCredentialsError)
+  })
+})
